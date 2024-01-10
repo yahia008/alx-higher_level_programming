f353dbf49c4d1bc6043570ce5a31fb7cfacd50b2
yahia008 <yahyatijjani99@gmail.com>
2024-01-10 15:18:47 -0800

    better

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_set = set_1.intersection(set_2)
-    return common_set
+    return ({element for element in set_1 if element in set_2})
