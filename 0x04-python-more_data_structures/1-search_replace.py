8cf339450e1ef2cffc284468362a591bdcd29e4c
yahia008 <yahyatijjani99@gmail.com>
2024-01-10 15:33:31 -0800

    fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 i#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     result_list = [replace if item == search else item for item in my_list]
-    return result_list
+    return (result_list)
