1ae20a4d6fca9399df13764c980c1aadb2ad3de6
yahia008 <yahyatijjani99@gmail.com>
2024-01-10 16:46:34 -0800

    go

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
-i#!/usr/bin/python3
+
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    result_list = [replace if item == search else item for item in my_list]
-    return (result_list)
+     return ([ele if ele != search else replace for ele in my_list])
+
