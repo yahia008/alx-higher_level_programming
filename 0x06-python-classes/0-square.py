ccfc2781ba7948d49a5e7a1529ff872b34d642cd
yahia008 <yahyatijjani99@gmail.com>
2024-01-24 02:36:32 -0800

    yello

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create a Square class.
+Which is class is currently inactive or does,
+not perform any actions at the moment.
+"""
+
+
+class Square:
+    """Represent a square."""
+    pass
