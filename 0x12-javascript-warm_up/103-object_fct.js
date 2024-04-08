48cd050a568c2bca4edc66f1a59a04eb046a65f0
yahia008 <yahyatijjani99@gmail.com>
2024-04-08 14:12:23 -0700

    fix

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,13 @@
+103-object_fct.js
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12
 };
-
 console.log(myObject);
-
 myObject.incr = function () {
   this.value++;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
