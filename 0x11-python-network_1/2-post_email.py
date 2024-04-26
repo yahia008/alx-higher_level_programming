53a2122dcf1f4f3929ad1c3d26276d2cfc70bff1
yahia008 <yahyatijjani99@gmail.com>
2024-04-26 09:59:44 -0700

    post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,15 +5,16 @@ URL with a given email.
 Usage: ./2-post_email.py <URL> <email>
 Shows the response body.
 """
-from urllib import request, parse
 import sys
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = parse.urlencode(values)
-    data = data.encode('ascii')
-    req = request.Request(sys.argv[1], data)
-    with request.urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
