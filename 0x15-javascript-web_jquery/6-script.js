a1b374d4d715f5237f819fd7ca0045a035d9a0b6
yahia008 <yahyatijjani99@gmail.com>
2024-05-29 08:54:01 -0700

    jqueryy

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+
+// Attach a click event listener to DIV#update_header using jQuery
+
+$('#update_header').click(function () {
+  // Update the text of the <header> element
+  $('header').text('New Header!!!');
+});
+
